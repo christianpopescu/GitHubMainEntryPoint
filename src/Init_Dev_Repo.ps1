e4d0bbd2b9373833e7feb008c2978fb2ac2b46e4
Christian Popescu <christian.popescu@outlook.com>
2023-01-26 08:55:08 +0100

    Add script to init repository

---
+++ src/Init_Dev_Repo.ps1
@@ -0,0 +1,6 @@
+$repo_location = "D:\ccp_wrks\LaSdeCSharpLibrary"
+
+New-Item -Path $repo_location -Name "src" -ItemType "directory"
+New-Item -Path $repo_location -Name "doc\worklog" -ItemType "directory"
+New-Item -Path $repo_location -Name "doc\swe-adoc" -ItemType "directory"
+New-Item -Path $repo_location -Name "doc\swe" -ItemType "directory"
